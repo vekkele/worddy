b4d204aa0ac3c233d76ee965eded92114e17334d
Vlad Keliy <vekkele@gmail.com>
2024-03-03 18:13:21 +0300

    🚧 Add basic echo app

---
+++ cmd/web/main.go
@@ -0,0 +1,9 @@
+package main
+
+type application struct{}
+
+func main() {
+	app := application{}
+
+	app.start()
+}
---
+++ cmd/web/server.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/labstack/echo/v4"
+
+func (app *application) start() {
+	e := echo.New()
+
+	e.GET("/", app.home)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
