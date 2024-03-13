dac6071ef0436e7d5b6a6d11037cb0d62baa581a
Vlad Keliy <vekkele@gmail.com>
2024-03-13 22:07:39 +0300

    💄 Add basic home page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,7 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":8080", "HTTP network address")
 
 	app := application{
 		addr: *addr,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./ui/view/**/*.templ"],
+  darkMode: "selector",
   theme: {
     extend: {}
   },
