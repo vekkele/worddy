8b564f040cd2e68d250f8510a4dde62c6dcd49ab
Vlad Keliy <vekkele@gmail.com>
2024-04-10 19:27:00 +0300

    🦺 Add simple form validation for signup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/vekkele/worddy/internal/models"
+	"github.com/vekkele/worddy/internal/validator"
 	"github.com/vekkele/worddy/ui/view/pages"
 )
 
@@ -14,7 +15,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signup(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, pages.Signup())
+	app.render(w, r, pages.Signup(pages.SignupForm{}))
 }
 
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +31,16 @@ func (app *application) signupPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//TODO: Validate form
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "Invalid email format")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 6), "password", "This field must be at least 6 characters")
+	form.CheckField(form.Password == form.PasswordConfirm, "password-confirm", "Passwords do not match")
+
+	if !form.Valid() {
+		app.render(w, r, pages.Signup(form))
+		return
+	}
 
 	user, err := app.users.Insert(r.Context(), form.Email, form.Password)
 	if err != nil {
