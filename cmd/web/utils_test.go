721b722d08b72b1ff039129bd69aaabf6f2922fb
Vlad Keliy <vekkele@gmail.com>
2024-07-13 12:22:35 +0300

    ✅ Add test for splitTranslations

---
+++ cmd/web/utils_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitTranslations(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       string
+		expected []string
+	}{
+		{
+			name:     "Empty",
+			in:       "",
+			expected: []string{},
+		},
+		{
+			name:     "One entry",
+			in:       "word",
+			expected: []string{"word"},
+		},
+		{
+			name:     "Several entries",
+			in:       "word,another,something",
+			expected: []string{"word", "another", "something"},
+		},
+		{
+			name:     "Several entries with spaces",
+			in:       "word,   another, something",
+			expected: []string{"word", "another", "something"},
+		},
+		{
+			name:     "Several entries with empty",
+			in:       "word,,  ,  another,     , something",
+			expected: []string{"word", "another", "something"},
+		},
+		{
+			name:     "Several entries, all empty",
+			in:       ",,  ,  ,     ,",
+			expected: []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := splitTranslations(tt.in)
+
+			assert.Equal(t, result, tt.expected)
+		})
+	}
+
+}
