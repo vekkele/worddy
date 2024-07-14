6ba4efc42998de02b6ecff5b59623207492f6022
Vlad Keliy <vekkele@gmail.com>
2024-07-14 15:17:05 +0300

    ✅ Refactor TestHome test cases

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -8,30 +8,50 @@ import (
 )
 
 func TestHome(t *testing.T) {
-	t.Run("Not logged in", func(t *testing.T) {
-		app := newTestApplication()
+	app := newTestApplication()
+	h := app.routes()
 
-		ts := newTestServer(t, app.routes())
-		defer ts.Close()
+	tests := []struct {
+		name              string
+		loggedIn          bool
+		expectedCode      int
+		expectedLocation  string
+		expectedElemenets []string
+	}{
+		{
+			name:              "No user logged in",
+			expectedCode:      http.StatusOK,
+			expectedElemenets: []string{`a[href="/user/signup"]`, `a[href="/user/login"]`},
+		},
+		{
+			name:             "User logged in",
+			loggedIn:         true,
+			expectedCode:     http.StatusSeeOther,
+			expectedLocation: "/dashboard",
+		},
+	}
 
-		code, _, doc := ts.get(t, "/")
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.loggedIn {
+				authedSessionMiddleware := LoadAndSaveMock(app.sessionManager, "authenticatedUserID", int64(1))
+				h = authedSessionMiddleware(h)
+			}
 
-		assert.Equal(t, http.StatusOK, code)
-		assert.True(t, doc.Find(`a[href="/user/signup"]`).Length() > 0, `Page must contain link to "/user/signup"`)
-		assert.True(t, doc.Find(`a[href="/user/login"]`).Length() > 0, `Page must contain link to "/user/login"`)
-	})
+			ts := newTestServer(t, h)
+			defer ts.Close()
 
-	t.Run("Logged in", func(t *testing.T) {
-		app := newTestApplication()
+			code, header, doc := ts.get(t, "/")
 
-		authedSessionMiddleware := LoadAndSaveMock(app.sessionManager, "authenticatedUserID", int64(1))
+			assert.Equal(t, tc.expectedCode, code)
 
-		ts := newTestServer(t, authedSessionMiddleware(app.routes()))
-		defer ts.Close()
+			if tc.expectedLocation != "" {
+				assert.Equal(t, tc.expectedLocation, header.Get("Location"))
+			}
 
-		code, header, _ := ts.get(t, "/")
-
-		assert.Equal(t, http.StatusSeeOther, code)
-		assert.Equal(t, "/dashboard", header.Get("Location"))
-	})
+			for _, selector := range tc.expectedElemenets {
+				assert.True(t, doc.Find(selector).Length() > 0, "Page must contain element for selector: %s", selector)
+			}
+		})
+	}
 }
