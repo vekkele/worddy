c5f9cb7b2712e08c589a477a4761a4a5823233ec
Vlad Keliy <vekkele@gmail.com>
2025-02-13 16:32:50 +0300

    🐛 Show browser local date in words table

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,14 +4,6 @@ import (
 	"time"
 )
 
-func HumanDate(t time.Time) string {
-	if t.IsZero() {
-		return ""
-	}
-
-	return t.UTC().Format("02 Jan 2006 at 15:04")
-}
-
 func CalculateNextReview(hoursToNext int32) time.Time {
 	currentTime := time.Now()
 	dur := time.Hour * time.Duration(hoursToNext)
